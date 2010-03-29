c52fa16b10c47f9656d3aa122187c91d64c00f6c
mac <mac@veryhappythings.co.uk>
2010-03-29 15:16:23 +0100

    One liner to grab keychain passwords from the command line

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -1,1 +1,3 @@
 function vcd() { mvim -c "cd $@" ;}
+function getpass() { security -q find-generic-password -g -l $@ "/Users/mac/Library/Keychains/billmonitor.keychain" 2>&1 | awk -F\" '/^password:/ {print $2}' | pbcopy ;}
+
