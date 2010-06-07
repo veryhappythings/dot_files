e7ff5b1951c89ba08d7a1c639f610664d721c439
mac <mac.s.c.chapman@gmail.com>
2010-06-07 22:40:22 +0100

    Added a grep alias

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,4 +1,6 @@
 alias ls="ls -G"
+alias grep="grep --color=auto"
+
 alias git=hub
 alias gitship="git pull optimor master && git push origin master && git push optimor master"
 alias gst="git status"
