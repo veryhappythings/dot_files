0916246db6f69c7436a15e6a79fc7e658a04fce5
mac <mac@veryhappythings.co.uk>
2011-06-09 14:59:13 +0100

    Removed an old alias

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -2,7 +2,6 @@ alias ls="ls -G"
 alias grep="grep --color=auto"
 
 alias git=hub
-alias gitship="git pull optimor master && git push origin master && git push optimor master"
 alias gst="git status"
 alias gbr="git branch"
 
