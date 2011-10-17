53ff37ee1e7ada69936772fdaada1c53d73663df
Mac <mac@veryhappythings.co.uk>
2011-10-17 16:44:23 +0100

    Much tmux tomfoolery

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/tmuxinator ~/.tmuxinator
+
+cp -r $PWD/bin/spotify ~/bin/spotify
+cp $PWD/bin/current_track ~/bin/current_track
+cp $PWD/bin/battery ~/bin/battery
