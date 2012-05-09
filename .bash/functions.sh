4b1480ddbf95ae77bc185d1515160ebc47d49dbd
Mac <mac@veryhappythings.co.uk>
2012-05-09 15:18:05 +0100

    Quicksilver and other tweaks

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -1,3 +1,4 @@
 function vcd() { mvim -c "cd $@" ;}
 function getpass() { security -q find-generic-password -g -l $@ "/Users/mac/Library/Keychains/billmonitor.keychain" 2>&1 | awk -F\" '/^password:/ {print $2}' | pbcopy ;}
 
+function nt() { nosetests osirium-$1/tests/unit ;}
