6b0eabdd2ba0565d1565fa9a8e9112682e265c8d
Mac <mac@veryhappythings.co.uk>
2014-10-08 10:17:56 +0100

    Flesh out install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-ln -s $PWD/tmux.conf ~/.tmux.conf
-ln -s $PWD/tmuxinator ~/.tmuxinator
+ln -sf ${PWD}/tmux.conf ~/.tmux.conf
+ln -sf ${PWD}/.ackrc ~/.ackrc
+ln -sf ${PWD}/.vimrc ~/.vimrc
+ln -sf ${PWD}/.gvimrc ~/.gvimrc
+ln -sf ${PWD}/.ackrc ~/.ackrc
+ln -sf ${PWD}/.bash_profile ~/.bash_profile
+ln -sf ${PWD}/.bash_profile ~/.bashrc
+ln -sf ${PWD}/.pythonrc.py ~/.pythonrc.py
 
-cp -r $PWD/bin/spotify ~/bin/spotify
-cp $PWD/bin/current_track ~/bin/current_track
-cp $PWD/bin/battery ~/bin/battery
+ln -s ${PWD}/tmuxinator ~/.tmuxinator
+ln -s ${PWD}/.bash ~/.bash
+ln -s ${PWD}/.vim ~/.vim
