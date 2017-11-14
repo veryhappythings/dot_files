dcdc58c4a6149aadba8bad44b36e4080cd9764b3
Mac Chapman <mac@veryhappythings.co.uk>
2017-11-14 10:55:44 +0000

    Remove dots

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 ln -sf ${PWD}/tmux.conf ~/.tmux.conf
-ln -sf ${PWD}/.ackrc ~/.ackrc
-ln -sf ${PWD}/.vimrc ~/.vimrc
-ln -sf ${PWD}/.gvimrc ~/.gvimrc
-ln -sf ${PWD}/.ackrc ~/.ackrc
-ln -sf ${PWD}/.bash_profile ~/.bash_profile
-ln -sf ${PWD}/.bash_profile ~/.bashrc
+ln -sf ${PWD}/ackrc ~/.ackrc
+ln -sf ${PWD}/vimrc ~/.vimrc
+ln -sf ${PWD}/gvimrc ~/.gvimrc
 ln -sf ${PWD}/.pythonrc.py ~/.pythonrc.py
 ln -sf ${PWD}/zshrc ~/.zshrc
 
 ln -s ${PWD}/tmuxinator ~/.tmuxinator
-ln -s ${PWD}/.bash ~/.bash
 ln -s ${PWD}/oh-my-zsh-custom ~/.oh-my-zsh-custom
 ln -s ${PWD}/.vim ~/.vim
