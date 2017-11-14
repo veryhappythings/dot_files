b239d7673c74c380c63c9a154695be439ce193b6
Mac Chapman <mac@veryhappythings.co.uk>
2017-11-14 10:56:10 +0000

    Remove pythonrc.py - I never used it

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 ln -sf ${PWD}/ackrc ~/.ackrc
 ln -sf ${PWD}/vimrc ~/.vimrc
 ln -sf ${PWD}/gvimrc ~/.gvimrc
-ln -sf ${PWD}/.pythonrc.py ~/.pythonrc.py
 ln -sf ${PWD}/zshrc ~/.zshrc
 
 ln -s ${PWD}/tmuxinator ~/.tmuxinator
