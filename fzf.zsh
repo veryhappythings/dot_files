5358492ce1affd77d124799cb945764bbfb0a3df
Mac Chapman <mac@veryhappythings.co.uk>
2018-01-16 14:58:13 +0000

    Respect ignore files

--- fzf.zsh
+++ fzf.zsh
@@ -15,6 +15,6 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 # Use ripgrep
 # -----------
 if [[ -f /usr/local/bin/rg ]]; then
-    export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+    export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
