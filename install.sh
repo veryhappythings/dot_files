52d0a40ac77d8e93c6cf0a700a142e375a0d3bbd
Mac Chapman <mac@veryhappythings.co.uk>
2020-08-31 21:14:15 +0100

    Add more files to install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@ ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 ln -sf ${PWD}/ackrc ~/.ackrc
 ln -sf ${PWD}/vimrc ~/.vimrc
 ln -sf ${PWD}/gvimrc ~/.gvimrc
+ln -sf ${PWD}/gitconfig ~/.gitconfig
+ln -sf ${PWD}/gitignore_global ~/.gitignore_global
 ln -sf ${PWD}/zshrc ~/.zshrc
 ln -sf ${PWD}/fzf.zsh ~/.fzf.zsh
+ln -sf ${PWD}/Xresources ~/.Xresources
 
 ln -s ${PWD}/tmuxinator ~/.tmuxinator
 ln -s ${PWD}/oh-my-zsh-custom ~/.oh-my-zsh-custom
