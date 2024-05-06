e994f782ed6cebf1fa5668f4c3bf47fccf0911e6
Mac Chapman <mac@veryhappythings.co.uk>
2024-05-06 15:09:36 +0100

    fix: remove mentions of /home

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,6 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/mac/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/mac/.fzf/bin"
-fi
-
-eval "$(fzf --zsh)"
-
 # Use ripgrep
 # -----------
-if [[ -f /usr/local/bin/rg ]]; then
+if [ -x "$(command -v rg)" ]; then
     export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
