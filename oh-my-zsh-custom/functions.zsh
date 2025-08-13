ca4567315c14953587f2cde21c55060068dce3be
Mac Chapman <mac@veryhappythings.co.uk>
2025-08-13 13:21:11 +0100

    feat: clip function

--- oh-my-zsh-custom/functions.zsh
+++ oh-my-zsh-custom/functions.zsh
@@ -1,1 +1,6 @@
-function vcd() { vim -c "cd $@" ;}
+function vcd() { nvim -c "cd $@" ;}
+
+# Clip interacts with the clipboard. If you pipe into it, it will copy to the clipboard. If you pipe out of it, it will paste from the clipboard.
+clip () {
+	[ -t 0 ] && pbpaste || pbcopy
+}
